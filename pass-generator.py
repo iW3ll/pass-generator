58b44f6983652ea45102f5e56accfa22e291cc35
Wesley Santos <84673115+iW3ll@users.noreply.github.com>
2024-01-27 23:45:42 -0300

    Create pass-generator.py

---
+++ pass-generator.py
@@ -0,0 +1,34 @@
+import random
+import datetime
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+
+for n in range (1, nr_letters + 1):
+  password.append(random.choice(letters))
+
+for n in range (1, nr_symbols + 1):
+  password.append(random.choice(symbols))
+
+for n in range (1, nr_numbers + 1):
+  password.append(random.choice(numbers))
+
+print(password)
+random.shuffle(password)
+print(password)
+
+passwordX = "" 
+for b in password:
+  passwordX += b
+
+x = datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+
+print(f"{passwordX}{x}")
